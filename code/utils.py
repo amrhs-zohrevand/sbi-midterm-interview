14bd19c1e0d50a9fa92735690ccc9da950d51434
Justin de Weert <j.s.deweert@gmail.com>
2025-05-21 14:29:54 +0200

    Changed Email

--- code/utils.py
+++ code/utils.py
@@ -81,7 +81,7 @@ def send_transcript_email(
         to_addr = recipient_email.strip()
         cc_addr = ""
         
-    bcc_addr = "zohrehvanda@liacs.leidenuniv.nl"
+    bcc_addr = "a.h.zohrehvand@liacs.leidenuniv.nl"
 
     subject = "Your Interview Transcript from Leiden University"
     
